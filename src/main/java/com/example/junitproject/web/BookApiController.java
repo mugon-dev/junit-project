755f67732b643f5086e1e087600ed6a98805c6f6
Conny <jmg41490400@gmail.com>
2022-10-16 17:43:19 +0900

    테스트환경 세팅 완료

--- src/main/java/com/example/junitproject/web/BookApiController.java
+++ src/main/java/com/example/junitproject/web/BookApiController.java
@@ -1,5 +1,8 @@
 package com.example.junitproject.web;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookApiController {
 
 }
--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -2,6 +2,26 @@ package com.example.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 class BookRepositoryTest {
 
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void 책등록_test(){
+        System.out.println("책등록 test");
+    }
+    // 2. 책 목록 보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 }
