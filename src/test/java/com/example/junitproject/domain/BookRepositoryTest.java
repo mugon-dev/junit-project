883de843916d2b348668c26d051360fc1f4cd57b
Conny <jmg41490400@gmail.com>
2022-10-16 18:02:46 +0900

    책등록_test 완료

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.example.junitproject.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,27 @@ class BookRepositoryTest {
     // 1. 책 등록
     @Test
     public void 책등록_test(){
-        System.out.println("책등록 test");
+        /**
+         * 컨트롤러 title,author -> BookSaveReqDto -> Service에 넘김
+         * -> Book 엔티티 변환 -> BookRepository.save(book)
+         * -> DB에 저장 (primary key 생성 = id 생성완료)
+         * -> save 메서드가 DB에 저장된 Book을 return (DB데이터와 동기화된 데이터)
+         */
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+
+        // when (테스트 실행)
+        // PS = persistance (영구적으로 저장됨 == 영속화된)
+        Book bookPS = bookRepository.save(book);
+
+        // then (검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
     }
     // 2. 책 목록 보기
 
