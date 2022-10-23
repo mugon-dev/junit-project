c47d08659badc8b59627c5d285c1f740ca4dd9ee
Conny <jmg41490400@gmail.com>
2022-10-23 17:15:05 +0900

    BeforeEach를 통한 책 목록, 단건 보기 테스트

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,6 +13,21 @@ class BookRepositoryTest {
 
     @Autowired // DI
     private BookRepository bookRepository;
+
+    // @BeforeAll // 테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비(){
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+        bookRepository.save(book);
+    }
+    // [데이터 준비+책 목록보기], [데이터 준비 + 책 단건보기]
+    // BeforeEach는 그 다음 테스트 완료까지 트랜잭션이 묶임
+
 
     // 1. 책 등록
     @Test
@@ -36,10 +53,34 @@ class BookRepositoryTest {
         // then (검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
-    }
+    } // 트랜잭션 종료 (저장된 데이터를 초기화)
     // 2. 책 목록 보기
+    @Test
+    public void 책목록보기_test(){
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+
+    }
 
     // 3. 책 한건 보기
+    @Test
+    public void 책한건보기_test(){
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then
+        assertEquals(title, bookPS.getTitle());
+    }
 
     // 4. 책 수정
 
