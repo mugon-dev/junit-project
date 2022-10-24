31bcbbd8b4fba91a6dbeab15d3cd796f01d48cb7
Conny <jmg41490400@gmail.com>
2022-10-24 10:12:55 +0900

    책 서비스레이어 완료

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -4,6 +4,9 @@ import com.example.junitproject.domain.Book;
 import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.web.dto.BookResDto;
 import com.example.junitproject.web.dto.BookSaveReqDto;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,21 +14,55 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class BookService {
+
     private final BookRepository bookRepository;
 
     // 1. 책등록
     @Transactional(rollbackFor = RuntimeException.class)
-    public BookResDto 책등록하기(BookSaveReqDto dto){
+    public BookResDto 책등록하기(BookSaveReqDto dto) {
         Book bookPS = bookRepository.save(dto.toEntity());
         return new BookResDto().toDto(bookPS);
     }
 
     // 2. 책목록보기
+    public List<BookResDto> 책목록보기() {
+        return bookRepository.findAll().stream()
+            .map(new BookResDto()::toDto)
+            .collect(Collectors.toList());
+    }
+
 
     // 3. 책한건보기
+    public BookResDto 책한건보기(Long id) {
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if (bookOP.isPresent()) {
+            return new BookResDto().toDto(bookOP.get());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }
+
 
     // 4. 책삭제
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책삭제하기(Long id) {
+        // 찾지 못했다고 해서 디비에서 오류나지 않음, null을 반환해줌
+        // deleteById는 null이면 IllegalArgumentException을 반환
+        bookRepository.deleteById(id);
+    }
 
     // 5. 책수정
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책수정하기(Long id, BookSaveReqDto dto) {
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if (bookOP.isPresent()) {
+            Book bookPS = bookOP.get();
+            bookPS.update(dto.getTitle(), dto.getAuthor());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+
+    } // 메서드 종료시에 더티체킹(flush)으로 update됨
 
 }
