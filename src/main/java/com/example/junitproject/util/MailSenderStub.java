c3ddb9a10e7b689db9cf12510e80e91456fbdad4
Conny <jmg41490400@gmail.com>
2022-10-24 14:46:40 +0900

    책 등록하기 통합 테스트

--- src/main/java/com/example/junitproject/util/MailSenderStub.java
+++ src/main/java/com/example/junitproject/util/MailSenderStub.java
@@ -1,11 +1,9 @@
 package com.example.junitproject.util;
 
-import org.springframework.stereotype.Component;
-
 /**
  * stub = 가정
  */
-@Component // IoC 컨테이너 등록
+// @Component // IoC 컨테이너 등록
 public class MailSenderStub implements MailSender{
 
     @Override
--- src/test/java/com/example/junitproject/web/BookApiControllerTest.java
+++ src/test/java/com/example/junitproject/web/BookApiControllerTest.java
@@ -1,7 +1,63 @@
 package com.example.junitproject.web;
 
-import static org.junit.jupiter.api.Assertions.*;
+// 통합테스트 (C,S,R)
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.junitproject.service.BookService;
+import com.example.junitproject.web.dto.request.BookSaveReqDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class BookApiControllerTest {
 
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private TestRestTemplate rt;
+
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init(){
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void saveBook_test() throws Exception {
+        // given
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("스프링1강");
+        bookSaveReqDto.setAuthor("겟인데어");
+        String body = om.writeValueAsString(bookSaveReqDto);
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body,headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.POST,request,String.class);
+
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+        assertThat(title).isEqualTo(bookSaveReqDto.getTitle());
+        assertThat(author).isEqualTo(bookSaveReqDto.getAuthor());
+    }
 }
